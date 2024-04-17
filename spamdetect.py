530664d37f907f3a581ebe465a3988b167b07d3c
Kunal NayaK <165077439+NayaKunal30@users.noreply.github.com>
2024-04-17 16:50:52 +0530

    ..

---
+++ spamdetect.py
@@ -0,0 +1,50 @@
+from random import choices
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from win32com.client import Dispatch
+import pythoncom
+import win32api
+
+pythoncom.CoInitialize()
+
+def speak(text):
+    speak = Dispatch(("SAPI.SpVoice"))
+    speak.Speak(text)
+
+model = pickle.load(open('model.pkl', 'rb'))
+cv = pickle.load(open('vectorizer.pkl', 'rb'))
+
+def main():
+    logo_col, title_col = st.columns([0.2, 1])  # Adjust the width of the logo column as needed
+    with logo_col:
+        st.image("CyberBlockLogo.png", width=100)  # Adjust width as needed
+
+    with title_col:
+        st.title(" :violet[SPAM EMAIL DETECTOR]")
+        st.write("Build By MAKSQUARE")
+
+    activities = ["SPAM EMAIL DETECTION", "About"]
+    choice = st.sidebar.selectbox("CHOOSE YOUR REQUIREMENT", activities)
+    if choice == "SPAM EMAIL DETECTION":
+        msg = st.text_input("Enter The Suspicious Email:")
+        if st.button("CHECK"):
+            print(msg)
+            print(type(msg))
+            data = [msg]
+            print(data)
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            if result[0] == 0:
+                st.success("This is Not A Spam Email")
+                speak("THANK GOD This is Not A Spam Email")
+            else:
+                st.error("This is A Spam Email")
+                speak("ALERT This is A Spam Email")
+    else:
+        st.write(':green[Spam Email Detection]')
+        st.write("Filter out Suspicious Emails")
+        st.write('Filter out you Suspicious By using this Spam Mail Detector Tool that will tell You Weather a Email is a Malicious Email or not. This Spam Email detector saves users valuable time and enhances productivity by ensuring that only relevant and legitimate messages reach their inboxes. Moreover, they serve as a frontline defense against various cyber threats, such as phishing attempts, malware distribution, and scams, thereby bolstering security and protecting sensitive information. Ultimately, This Email Spam detector fosters a positive email experience by delivering a clean and trustworthy inbox, thereby enhancing user satisfaction and trust in email communication.')
+
+main()
