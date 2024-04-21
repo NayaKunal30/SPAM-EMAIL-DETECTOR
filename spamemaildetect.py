f4bebd6bc7f9a136f0478bb168f8601fb88fb425
Kunal NayaK <165077439+NayaKunal30@users.noreply.github.com>
2024-04-21 18:40:16 +0530

    changed some css

--- spamemaildetect.py
+++ spamemaildetect.py
@@ -15,30 +15,45 @@ import numpy as np
 # def speak(text):
 #     speak = Dispatch(("SAPI.SpVoice"))
 #     speak.Speak(text)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stTitle {margin-bottom: -10px;}
+            .stMarkdown {margin-top: -20px;}
+            .logo-container {
+            display: flex;
+            align-items: center;
+            }
+            
+            </style>
+            """
 
+st.markdown(hide_st_style, unsafe_allow_html=True)
 # Load the model and CountVectorizer
 model = pickle.load(open('model.pkl', 'rb'))
 cv = pickle.load(open('vectorizer.pkl', 'rb'))
 
 def main():
     # Adjust the width of the logo column as needed
-    logo_col, title_col = st.columns([0.2, 1])  
+    logo_col, title_col = st.columns([0.2,1])  
 
     with logo_col:
         # Adjust width as needed
         st.image("CyberBlockLogo.png", width=100)  
 
     with title_col:
+        
         st.title(" :violet[SPAM EMAIL DETECTOR]")
-        st.write("Build By MAKSQUARE")
+        st.write(" :green[Build By MAKSQUARE]")
 
-    activities = ["SPAM EMAIL DETECTION", "About"]
+    activities = ["SPAM EMAIL DETECTION", "About EMAIL SPAM DETECTOR"]
     choice = st.sidebar.selectbox("CHOOSE YOUR REQUIREMENT", activities)
-    
     if choice == "SPAM EMAIL DETECTION":
         msg = st.text_input("Enter The Suspicious Email:")
         
-        if st.button("CHECK"):
+        if st.button(":red[CHECK]"):
             data = [msg]
             vec = cv.transform(data).toarray()
             result = model.predict(vec)
@@ -52,8 +67,11 @@ def main():
                 # speak("ALERT This is A Spam Email")
                 
     else:
-        st.write(':green[Spam Email Detection]')
+        st.write("        ")
+        st.write(':red[Spam Email Detection]')
+        st.write("                ")
         st.write("Filter out Suspicious Emails")
+        st.write("                  ")
         st.write('Filter out you Suspicious By using this Spam Mail Detector Tool that will tell You Weather a Email is a Malicious Email or not. This Spam Email detector saves users valuable time and enhances productivity by ensuring that only relevant and legitimate messages reach their inboxes. Moreover, they serve as a frontline defense against various cyber threats, such as phishing attempts, malware distribution, and scams, thereby bolstering security and protecting sensitive information. Ultimately, This Email Spam detector fosters a positive email experience by delivering a clean and trustworthy inbox, thereby enhancing user satisfaction and trust in email communication.')
 
 main()
